e763f5ece837dced312d08ae33f42282d4f7fe15
git-k <kgunu93@gmail.com>
2023-03-20 21:47:50 +0900

    add cors

--- a-002-clone-app/clone-reddit-app/server/src/server.ts
+++ a-002-clone-app/clone-reddit-app/server/src/server.ts
@@ -1,14 +1,19 @@
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
+import authRoutes from "./routes/auth";
 
 const app = express();
 const PORT = 4000;
+const origin = "http://localhost:3000";
+app.use(cors({ origin }));
 
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (_, res) => res.send("running"));
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Server running at http://localhost:${PORT}`);
