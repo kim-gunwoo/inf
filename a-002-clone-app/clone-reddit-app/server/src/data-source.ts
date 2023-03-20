e6537b183c7dfe67710d91cca8ba4b13e114a67f
git-k <kgunu93@gmail.com>
2023-03-20 21:56:26 +0900

    modify datasouce

--- a-002-clone-app/clone-reddit-app/server/src/data-source.ts
+++ a-002-clone-app/clone-reddit-app/server/src/data-source.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-// import { User } from "./entities/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -11,8 +10,7 @@ export const AppDataSource = new DataSource({
   database: "postgres",
   synchronize: true,
   logging: false,
-  //   entities: [User],
-  entities: ["src/entity/**/*.ts"],
+  entities: ["src/entities/**/*.ts"],
   migrations: [],
   subscribers: [],
 });
