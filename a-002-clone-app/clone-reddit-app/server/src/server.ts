d81c7e84cb8f5e80b22975ebb19c87320004701e
git-k <kgunu93@gmail.com>
2023-03-28 22:51:57 +0900

    modify server

--- a-002-clone-app/clone-reddit-app/server/src/server.ts
+++ a-002-clone-app/clone-reddit-app/server/src/server.ts
@@ -1,12 +1,16 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
 import { AppDataSource } from "./data-source";
 import authRoutes from "./routes/auth";
 
+dotenv.config({ path: ".env.development" });
+
 const app = express();
-const PORT = 4000;
-const origin = "http://localhost:3000";
+const PORT = process.env.PORT;
+
+const origin = process.env.ORIGIN;
 app.use(cors({ origin, credentials: true }));
 
 app.use(express.json());
@@ -16,7 +20,7 @@ app.get("/", (_, res) => res.send("running"));
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, async () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at ${process.env.APP_URL}`);
   AppDataSource.initialize()
     .then(() => {
       console.log("database init");
