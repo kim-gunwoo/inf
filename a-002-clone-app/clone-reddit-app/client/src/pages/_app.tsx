2f0d293b950303ac026b6cdefccc1a5f5f704bfc
git-k <kgunu93@gmail.com>
2023-03-31 06:52:13 +0900

    add provider

--- a-002-clone-app/clone-reddit-app/client/src/pages/_app.tsx
+++ a-002-clone-app/clone-reddit-app/client/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "@/context/auth";
 import "@/styles/globals.css";
 import Axios from "axios";
 import type { AppProps } from "next/app";
@@ -5,5 +6,9 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
 
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
