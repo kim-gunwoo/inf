09cd661b57a45668fb6765757b9db4b6fcbc8671
git-k <kgunu93@gmail.com>
2023-04-02 21:02:21 +0900

    add subs route

---
+++ a-002-clone-app/clone-reddit-app/server/src/routes/subs.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../entities/User";
+
+const router = Router();
+
+const createSub = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, title, description } = req.body;
+
+  const token = req.cookies.token;
+  if (!token) return next();
+
+  const { username }: any = jwt.verify(token, process.env.JWT_SECRET!);
+
+  const user = await User.findOneBy({ username });
+
+  if (!user) throw new Error("Unauthenticated");
+};
+
+router.post("/", createSub);
+
+export default router;
