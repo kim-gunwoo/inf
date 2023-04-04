9811fcf54f0450fb7d898f1b3652c8aae8f5ab84
git-k <kgunu93@gmail.com>
2023-04-04 20:59:53 +0900

    axios setting

--- a-002-clone-app/clone-reddit-app/client/src/pages/_app.tsx
+++ a-002-clone-app/clone-reddit-app/client/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+  Axios.defaults.withCredentials = true;
 
   return (
     <AuthProvider>
