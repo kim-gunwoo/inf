a0aa22a8883d45695f7eaeac11182ac618f4c36f
git-k <kgunu93@gmail.com>
2023-04-05 22:24:59 +0900

    add create subs

--- a-002-clone-app/clone-reddit-app/server/src/routes/subs.ts
+++ a-002-clone-app/clone-reddit-app/server/src/routes/subs.ts
@@ -1,22 +1,53 @@
-import { NextFunction, Request, Response, Router } from "express";
-import jwt from "jsonwebtoken";
+import { Request, Response, Router } from "express";
+import userMiddleware from "../middlewares/user";
+import authMiddleware from "../middlewares/auth";
+import { isEmpty } from "class-validator";
+import { AppDataSource } from "../data-source";
+import Sub from "../entities/Sub";
 import { User } from "../entities/User";
 
 const router = Router();
 
-const createSub = async (req: Request, res: Response, next: NextFunction) => {
+const createSub = async (req: Request, res: Response) => {
   const { name, title, description } = req.body;
 
-  const token = req.cookies.token;
-  if (!token) return next();
+  try {
+    let errors: any = {};
 
-  const { username }: any = jwt.verify(token, process.env.JWT_SECRET!);
+    if (isEmpty(name)) errors.name = "이름은 비워둘 수 없습니다.";
+    if (isEmpty(title)) errors.title = "제목은 비워두 수 없습니다.";
 
-  const user = await User.findOneBy({ username });
+    const sub = await AppDataSource.getRepository(Sub)
+      .createQueryBuilder("sub")
+      .where("lower(sub.name) = :name", { name: name.toLowerCase() })
+      .getOne();
 
-  if (!user) throw new Error("Unauthenticated");
+    if (sub) errors.name = "서브가 이미 존재합니다.";
+    if (Object.keys(errors).length > 0) {
+      throw errors;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
+  }
+
+  try {
+    const user: User = res.locals.user;
+
+    const sub = new Sub();
+    sub.name = name;
+    sub.title = title;
+    sub.description = description;
+    sub.user = user;
+
+    await sub.save();
+    return res.json(sub);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
+  }
 };
 
-router.post("/", createSub);
+router.post("/", userMiddleware, authMiddleware, createSub);
 
 export default router;
