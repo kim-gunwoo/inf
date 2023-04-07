c3cfee26292c3cdef4467e06355b13f5dad8dbab
git-k <kgunu93@gmail.com>
2023-04-07 20:47:30 +0900

    add navbar

--- a-002-clone-app/clone-reddit-app/client/src/pages/_app.tsx
+++ a-002-clone-app/clone-reddit-app/client/src/pages/_app.tsx
@@ -1,15 +1,24 @@
+import NavBar from "@/components/Navbar";
 import { AuthProvider } from "@/context/auth";
 import "@/styles/globals.css";
 import Axios from "axios";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
   Axios.defaults.withCredentials = true;
 
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      {!authRoute && <NavBar />}
+      <div className={authRoute ? "" : "pt-12 bg-gray-200 min-h-screen"}>
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   );
 }
