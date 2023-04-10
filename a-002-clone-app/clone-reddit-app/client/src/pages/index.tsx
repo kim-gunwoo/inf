a16e86eec12910bc3f251d103032d5252ce14696
git-k <kgunu93@gmail.com>
2023-04-10 20:49:19 +0900

    modify home

--- a-002-clone-app/clone-reddit-app/client/src/pages/index.tsx
+++ a-002-clone-app/clone-reddit-app/client/src/pages/index.tsx
@@ -1,9 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,9 +10,39 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </main>
+      <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+        {/* 포스트 리스트 */}
+        <div className="w-full md:mr-3 md:w-8/12">
+          {/* 
+          TODO 
+          */}
+        </div>
+
+        {/* 사이드바 */}
+        <div className="hidden w-4/12 ml-3 md:block">
+          <div className="bg-white border rounded">
+            <div className="p-4 border-b">
+              <p className="text-lg font-semibold text-center">상위 커뮤니티</p>
+            </div>
+
+            {/* 커뮤니티 리스트 */}
+            <div>
+              {/* {
+                TODO
+              } */}
+            </div>
+
+            <div className="w-full py-6 text-center">
+              <Link
+                className="w-full p-2 text-center text-white bg-gray-400 rounded"
+                href="/subs/create"
+              >
+                커뮤니티 만들기
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
