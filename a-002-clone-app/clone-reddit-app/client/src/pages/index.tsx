26e12e4c71f57de11f14be7abc287386c970e4c2
git-k <kgunu93@gmail.com>
2023-04-14 22:07:42 +0900

    add images cconfig and auth

--- a-002-clone-app/clone-reddit-app/client/src/pages/index.tsx
+++ a-002-clone-app/clone-reddit-app/client/src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { useAuthState } from "@/context/auth";
 import { Sub } from "@/types";
 import axios from "axios";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import useSWR from "swr";
 
 export default function Home() {
+  const { authenticated } = useAuthState();
   const fetcher = async (url: string) => {
     return await axios.get(url).then((res) => res.data);
   };
@@ -22,11 +25,7 @@ export default function Home() {
       </Head>
       <div className="flex max-w-5xl px-4 pt-5 mx-auto">
         {/* 포스트 리스트 */}
-        <div className="w-full md:mr-3 md:w-8/12">
-          {/* 
-          TODO 
-          */}
-        </div>
+        <div className="w-full md:mr-3 md:w-8/12"></div>
 
         {/* 사이드바 */}
         <div className="hidden w-4/12 ml-3 md:block">
@@ -37,18 +36,40 @@ export default function Home() {
 
             {/* 커뮤니티 리스트 */}
             <div>
-              {/* {
-                TODO
-              } */}
+              {topSubs?.map((sub) => (
+                <div
+                  key={sub.name}
+                  className="flex items-center px-4 py-2 text-xs border-b"
+                >
+                  <Link href={`/r/${sub.name}`}>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                  <Link
+                    className="ml-2 font-bold hover:cursor-pointer"
+                    href={`/r/${sub.name}`}
+                  >
+                    /r/{sub.name}
+                  </Link>
+                  <p className="ml-auto font-md">{sub.postCount}</p>
+                </div>
+              ))}
             </div>
 
             <div className="w-full py-6 text-center">
-              <Link
-                className="w-full p-2 text-center text-white bg-gray-400 rounded"
-                href="/subs/create"
-              >
-                커뮤니티 만들기
-              </Link>
+              {authenticated && (
+                <Link
+                  className="w-full p-2 text-center text-white bg-gray-400 rounded"
+                  href="/subs/create"
+                >
+                  커뮤니티 만들기
+                </Link>
+              )}
             </div>
           </div>
         </div>
