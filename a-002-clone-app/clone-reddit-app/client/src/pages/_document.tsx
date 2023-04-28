ddb933038532f8e64e23c7b52fe3d2e550e48b4f
git-k <kgunu93@gmail.com>
2023-04-28 23:04:29 +0900

    add fontawesome cdn

--- a-002-clone-app/clone-reddit-app/client/src/pages/_document.tsx
+++ a-002-clone-app/clone-reddit-app/client/src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://use.fontawesome.com/releases/v6.1.1/js/all.js"
+          integrity="sha384-xBXmu0dk1bEoiwd71wOonQLyH+VpgR1XcDH3rtxrLww5ajNTuMvBdL5SOiFZnNdp"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
