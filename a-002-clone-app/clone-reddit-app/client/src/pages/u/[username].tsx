a6addc8939682f24965eca7750efb77396a2844f
git-k <kgunu93@gmail.com>
2023-05-16 21:42:34 +0900

    add username page

--- a-002-clone-app/clone-reddit-app/client/src/pages/u/[username].tsx
+++ a-002-clone-app/clone-reddit-app/client/src/pages/u/[username].tsx
@@ -1,3 +1,14 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
 const UserPage = () => {
+  const router = useRouter();
+  const username = router.query.username;
+  const { data, error } = useSWR(username ? `/users/${username}` : null);
+
+  if (!data) return null;
+
   return <div>user page</div>;
 };
+
+export default UserPage;
