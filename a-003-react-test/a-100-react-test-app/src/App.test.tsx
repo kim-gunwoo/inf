35050802d43ec00137d9f676ab0f796fffee7ea6
git-k <kgunu93@gmail.com>
2023-06-14 21:54:52 +0900

    add change action

--- a-003-react-test/a-100-react-test-app/src/App.test.tsx
+++ a-003-react-test/a-100-react-test-app/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 // test("renders learn react link", () => {
@@ -36,4 +36,20 @@ test("plus button has correct text", () => {
   render(<App />);
   const plusButtonElement = screen.getByTestId("plus-button");
   expect(plusButtonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent("1");
+});
+
+test("When the - button is pressed, the counter changes to -1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent("-1");
 });
