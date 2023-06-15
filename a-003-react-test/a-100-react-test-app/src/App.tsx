6e9361d5170996abb6f764444335bb403b863e69
git-k <kgunu93@gmail.com>
2023-06-16 06:57:09 +0900

    add disabled action

--- a-003-react-test/a-100-react-test-app/src/App.tsx
+++ a-003-react-test/a-100-react-test-app/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
@@ -12,12 +13,14 @@ function App() {
           <button
             data-testid="minus-button"
             onClick={() => setCounter((count) => count - 1)}
+            disabled={disabled}
           >
             -
           </button>
           <button
             data-testid="plus-button"
             onClick={() => setCounter((count) => count + 1)}
+            disabled={disabled}
           >
             +
           </button>
@@ -26,6 +29,7 @@ function App() {
           <button
             data-testid="on/off-button"
             style={{ backgroundColor: "blue" }}
+            onClick={() => setDisabled((prev) => !prev)}
           >
             on/off
           </button>
