d6dd5abe490727b5af171ba3abef3cedb7abb3af
git-k <kgunu93@gmail.com>
2023-06-21 22:06:08 +0900

    add eslint

--- a-003-react-test/a-150-react-shop-test/react-shop-test-app/src/App.test.tsx
+++ a-003-react-test/a-150-react-shop-test/react-shop-test-app/src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  const lintTest = screen.getByRole("button", { name: "lintTest" });
+  // expect(lintTest.textContent).toBe("lintTest");
+  expect(lintTest).toHaveTextContent("lintTest");
 });
