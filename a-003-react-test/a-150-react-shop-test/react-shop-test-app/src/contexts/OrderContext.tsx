f883ee92fae2c3eb8aa4dd44df6fcf15d5411dec
git-k <kgunu93@gmail.com>
2023-07-10 21:28:00 +0900

    feat: add total

--- a-003-react-test/a-150-react-shop-test/react-shop-test-app/src/contexts/OrderContext.tsx
+++ a-003-react-test/a-150-react-shop-test/react-shop-test-app/src/contexts/OrderContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 
 type IState = readonly [
   {
@@ -40,11 +40,44 @@ interface IProps {
   children: React.ReactNode;
 }
 
+const pricePerItem = {
+  products: 1000,
+  options: 500,
+};
+
+function calculateSubtotal(
+  orderType: "products" | "options",
+  orderCounts: IOrderCounts
+) {
+  let optionCount = 0;
+  for (const count of orderCounts[orderType].values()) {
+    optionCount += count;
+  }
+  return optionCount * pricePerItem[orderType];
+}
+
 export function OrderContextProvider({ children }: IProps) {
   const [orderCounts, setOrderCounts] = useState({
     products: new Map(),
     options: new Map(),
   });
+
+  const [totals, setTotals] = useState({
+    products: 0,
+    options: 0,
+    total: 0,
+  });
+
+  useEffect(() => {
+    const productsTotal = calculateSubtotal("products", orderCounts);
+    const optionsTotal = calculateSubtotal("options", orderCounts);
+    const total = productsTotal + optionsTotal;
+    setTotals({
+      products: productsTotal,
+      options: optionsTotal,
+      total,
+    });
+  }, [orderCounts]);
 
   const value = useMemo(() => {
     function updateItemCount(
@@ -61,8 +94,8 @@ export function OrderContextProvider({ children }: IProps) {
       setOrderCounts(newOrderCounts);
     }
 
-    return [{ ...orderCounts }, updateItemCount] as const;
-  }, [orderCounts]);
+    return [{ ...orderCounts, totals }, updateItemCount] as const;
+  }, [orderCounts, totals]);
 
   return (
     <OrderContext.Provider value={value}>{children}</OrderContext.Provider>
