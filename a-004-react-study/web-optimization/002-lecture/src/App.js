78396147e59ac9f7140d85b6761f6d80e298b21f
git-k <kgunu93@gmail.com>
2023-10-12 22:14:29 +0900

    add lazy preload

--- a-004-react-study/web-optimization/002-lecture/src/App.js
+++ a-004-react-study/web-optimization/002-lecture/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Suspense, lazy } from 'react'
+import React, { useState, Suspense, lazy, useEffect } from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
@@ -6,16 +6,37 @@ import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
 // import ImageModal from './components/ImageModal'
 
-const LazyImageModal = lazy(()=> import('./components/ImageModal'))
+// const LazyImageModal = lazy(()=> import('./components/ImageModal'))
+
+function LazyWithPreload(importFunction) {
+    const Component = React.lazy(importFunction)
+    Component.preload = importFunction
+    return Component;
+}
+
+const LazyImageModal = LazyWithPreload(()=> import('./components/ImageModal'))
 
 function App() {
     const [showModal, setShowModal] = useState(false)
+
+    // const handleMouseEnter = () => {
+    //     // 마우스 호버시 import
+    //     const Component =  import('./components/ImageModal');
+    // }
+
+    useEffect(() => {
+        // 마운트시 로딩
+        // const Component =  import('./components/ImageModal');
+        LazyImageModal.preload()
+    }, [])
+    
 
     return (
         <div className="App">
             <Header />
             <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
+            {/* <ButtonModal onClick={() => { setShowModal(true) }} onMouseEnter={handleMouseEnter}>올림픽 사진 보기</ButtonModal> */}
+            <ButtonModal onClick={() => { setShowModal(true) }} >올림픽 사진 보기</ButtonModal>
             <SurveyChart />
             <Footer />
             {/* {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null} */}
