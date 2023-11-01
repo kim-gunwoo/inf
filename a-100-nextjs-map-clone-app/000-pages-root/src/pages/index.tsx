a78fc7448a7240e6cb8eee0e1e45bf536f4d1a1f
git-k <kgunu93@gmail.com>
2023-11-01 21:21:39 +0900

    modify home add header

--- a-100-nextjs-map-clone-app/000-pages-root/src/pages/index.tsx
+++ a-100-nextjs-map-clone-app/000-pages-root/src/pages/index.tsx
@@ -1,9 +1,34 @@
 import HeaderComponent from '@/components/common/Header';
+import styles from '../styles/header.module.scss';
+import Link from 'next/link';
+import { AiOutlineShareAlt } from 'react-icons/ai';
+import { VscFeedback } from 'react-icons/vsc';
 
 export default function Home() {
   return (
     <div>
-      <HeaderComponent />
+      <HeaderComponent
+        rightElements={[
+          <button
+            key={'copy'}
+            className={styles.box}
+            style={{ marginRight: 8 }}
+            onClick={() => {
+              alert('copy');
+            }}
+          >
+            <AiOutlineShareAlt size={20} color="#444444" />
+          </button>,
+          <Link
+            key="link"
+            className={styles.box}
+            href="/feedback"
+            aria-label="피드백 페이지로 이동"
+          >
+            <VscFeedback size={20} color="#444444" />
+          </Link>,
+        ]}
+      />
     </div>
   );
 }
