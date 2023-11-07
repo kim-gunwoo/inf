e99ca936eee333d5fddfeca564cada572fc668b8
git-k <kgunu93@gmail.com>
2023-11-07 21:35:05 +0900

    add usestores getstaticprops

--- a-100-nextjs-map-clone-app/000-pages-root/src/pages/index.tsx
+++ a-100-nextjs-map-clone-app/000-pages-root/src/pages/index.tsx
@@ -4,8 +4,22 @@ import Link from 'next/link';
 import { AiOutlineShareAlt } from 'react-icons/ai';
 import { VscFeedback } from 'react-icons/vsc';
 import MapSection from '@/components/home/MapSection';
+import { Store } from '@/types/store';
+import { NextPage } from 'next';
+import { useEffect } from 'react';
+import useStores from '@/hooks/useStores';
 
-export default function Home() {
+interface Props {
+  stores: Store[];
+}
+
+const Home: NextPage<Props> = ({ stores }) => {
+  const { initializeStores } = useStores();
+
+  useEffect(() => {
+    initializeStores(stores);
+  }, [initializeStores, stores]);
+
   return (
     <>
       <HeaderComponent
@@ -35,4 +49,15 @@ export default function Home() {
       </main>
     </>
   );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  const stores = (await import('../../public/stores.json')).default;
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60, // 1 hours
+  };
 }
