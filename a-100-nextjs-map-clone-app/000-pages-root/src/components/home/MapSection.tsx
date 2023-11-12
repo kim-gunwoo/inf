439e3da663b41f4990d20ea6ac7551bbbd69bd06
git-k <kgunu93@gmail.com>
2023-11-12 19:24:59 +0900

    add marker

--- a-100-nextjs-map-clone-app/000-pages-root/src/components/home/MapSection.tsx
+++ a-100-nextjs-map-clone-app/000-pages-root/src/components/home/MapSection.tsx
@@ -1,12 +1,20 @@
+import { NaverMap } from '@/types/map';
 import Map from './Map';
+import useMap from '@/hooks/useMap';
+import Markers from './Markers';
 
 const MapSection = () => {
+  /** onLoadMap */
+  const { initializeMap } = useMap();
+  const onLoadMap = (map: NaverMap) => {
+    initializeMap(map);
+  };
+
   return (
-    <Map
-      onLoad={() => {
-        console.log('load');
-      }}
-    />
+    <>
+      <Map onLoad={onLoadMap} />
+      <Markers />
+    </>
   );
 };
 
---
+++ a-100-nextjs-map-clone-app/000-pages-root/src/components/home/Markers.tsx
@@ -0,0 +1,59 @@
+import { MAP_KEY } from '@/hooks/useMap';
+import { STORE_KEY } from '@/hooks/useStores';
+import { ImageIcon, NaverMap } from '@/types/map';
+import { Store } from '@/types/store';
+import useSWR from 'swr';
+import Marker from './Marker';
+
+const Markers = () => {
+  const { data: map } = useSWR<NaverMap>(MAP_KEY);
+  const { data: stores } = useSWR<Store[]>(STORE_KEY);
+
+  if (!map || !stores) {
+    return null;
+  }
+
+  return (
+    <>
+      {stores.map((store) => {
+        return (
+          <Marker
+            key={store.nid}
+            map={map}
+            coordinates={store.coordinates}
+            icon={generateStoreMarkerIcon(store.season)}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const MARKER_HEIGHT = 64;
+const MARKER_WIDTH = 54;
+const NUMBER_OF_MARKER = 13;
+const SCALE = 2 / 3;
+
+const SCALED_MARKER_WIDTH = MARKER_WIDTH * SCALE;
+const SCALED_MARKER_HEIGHT = MARKER_HEIGHT * SCALE;
+
+const CHUNSICK = (210 * 1) / 6;
+
+export function generateStoreMarkerIcon(markerIndex: number): ImageIcon {
+  /** https://navermaps.github.io/maps.js.ncp/docs/tutorial-8-marker-retina-sprite.example.html */
+  return {
+    // url: 'images/markers.png',
+    // size: new naver.maps.Size(SCALED_MARKER_WIDTH, SCALED_MARKER_HEIGHT),
+    // origin: new naver.maps.Point(SCALED_MARKER_WIDTH * markerIndex, 0),
+    // scaledSize: new naver.maps.Size(
+    //   SCALED_MARKER_WIDTH * NUMBER_OF_MARKER,
+    //   SCALED_MARKER_HEIGHT
+    // ),
+    url: 'logo.png',
+    size: new naver.maps.Size(CHUNSICK, CHUNSICK),
+    origin: new naver.maps.Point(0, 0),
+    scaledSize: new naver.maps.Size(CHUNSICK, CHUNSICK),
+  };
+}
+
+export default Markers;
