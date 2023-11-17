c61d9ab03b486e8464bbb71b63fbd451ced843c1
git-k <kgunu93@gmail.com>
2023-11-17 23:24:17 +0900

    feat zoom center

--- a-100-nextjs-map-clone-app/000-pages-root/src/components/home/Header.tsx
+++ a-100-nextjs-map-clone-app/000-pages-root/src/components/home/Header.tsx
@@ -6,18 +6,32 @@ import styles from '../../styles/header.module.scss';
 import { AiOutlineShareAlt } from 'react-icons/ai';
 import { VscFeedback } from 'react-icons/vsc';
 import HeaderComponent from '../common/Header';
+import useMap from '@/hooks/useMap';
+import { useRouter } from 'next/router';
+import copy from 'copy-to-clipboard';
 
 const HomeHeader = () => {
+  const { resetMapOptions, getMapOptions } = useMap();
+
+  const router = useRouter();
+  const replaceAndCopyUrl = useCallback(() => {
+    const mapOptions = getMapOptions();
+    const query = `/?zoom=${mapOptions.zoom}&lat=${mapOptions.center[0]}&lng=${mapOptions.center[1]}`;
+
+    router.replace(query);
+    // copy
+    copy(location.origin + query);
+  }, [router, getMapOptions]);
+
   return (
     <HeaderComponent
+      onClickLogo={resetMapOptions}
       rightElements={[
         <button
           key={'copy'}
           className={styles.box}
           style={{ marginRight: 8 }}
-          onClick={() => {
-            alert('copy');
-          }}
+          onClick={replaceAndCopyUrl}
         >
           <AiOutlineShareAlt size={20} color="#444444" />
         </button>,
--- a-100-nextjs-map-clone-app/000-pages-root/src/components/home/MapSection.tsx
+++ a-100-nextjs-map-clone-app/000-pages-root/src/components/home/MapSection.tsx
@@ -1,10 +1,38 @@
 import { NaverMap } from '@/types/map';
 import Map from './Map';
-import useMap from '@/hooks/useMap';
+import useMap, { INITIAL_CENTER, INITIAL_ZOOM } from '@/hooks/useMap';
 import Markers from './Markers';
 import useCurrentStore from '@/hooks/useCurrentStore';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
+import { Coordinates } from '@/types/store';
 
 const MapSection = () => {
+  /** url query 로부터 initial zoom, center 값 설정 */
+  const router = useRouter();
+  /**
+   * router.asPath === '/?zoom={}&lat={}&lng={}'
+   * https://developer.mozilla.org/ko/docs/Web/API/URLSearchParams
+   */
+
+  const query = useMemo(
+    () => new URLSearchParams(router.asPath.slice(1)),
+    // [router.asPath]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+  const initialZoom = useMemo(
+    () => (query.get('zoom') ? Number(query.get('zoom')) : INITIAL_ZOOM),
+    [query]
+  );
+  const initialCenter = useMemo<Coordinates>(
+    () =>
+      query.get('lat') && query.get('lng')
+        ? [Number(query.get('lat')), Number(query.get('lng'))]
+        : INITIAL_CENTER,
+    [query]
+  );
+
   /** onLoadMap */
   const { initializeMap } = useMap();
   const { clearCurrentStore } = useCurrentStore();
@@ -16,7 +44,11 @@ const MapSection = () => {
 
   return (
     <>
-      <Map onLoad={onLoadMap} />
+      <Map
+        onLoad={onLoadMap}
+        initialZoom={initialZoom}
+        initialCenter={initialCenter}
+      />
       <Markers />
     </>
   );
