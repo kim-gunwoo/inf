45ac9b043c91f63a92b700b98494a2587a2e8ee9
git-k <kgunu93@gmail.com>
2023-12-10 19:11:02 +0900

    feat feedback

--- a-100-nextjs-map-clone-app/000-pages-root/src/pages/feedback.tsx
+++ a-100-nextjs-map-clone-app/000-pages-root/src/pages/feedback.tsx
@@ -1,15 +1,43 @@
+import FeedbackSection from '@/components/feedback/FeedbackSection';
 import HomeHeader from '@/components/home/Header';
+import { getFeedbackListFromFirestore } from '@/firebase/feedback';
+import { Feedback } from '@/types/feedback';
+import { GetServerSideProps } from 'next';
+
 import { NextSeo } from 'next-seo';
 
-export default function Feedback() {
+interface Props {
+  initialFeedbackList: Feedback[];
+}
+
+export default function Feedback({ initialFeedbackList }: Props) {
   return (
-    <div>
+    <>
       <NextSeo
         title="피드백"
         description="매장 지도 서비스에 대한 피드백을 받습니다."
         canonical="my domain address /feedback"
       />
       <HomeHeader />
-    </div>
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          touchAction: 'pinch-zoom',
+        }}
+      >
+        <FeedbackSection initialFeedbackList={initialFeedbackList} />
+      </main>
+    </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      initialFeedbackList: await getFeedbackListFromFirestore(),
+    },
+  };
+};
