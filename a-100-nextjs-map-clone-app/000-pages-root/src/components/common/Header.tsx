16edf92de0b42b9acffae9aa886e202d0ce12797
git-k <kgunu93@gmail.com>
2023-12-11 21:13:10 +0900

    feat: 커스텀 anchor

--- a-100-nextjs-map-clone-app/000-pages-root/src/components/common/Header.tsx
+++ a-100-nextjs-map-clone-app/000-pages-root/src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ComponentPropsWithoutRef, ForwardedRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../../styles/header.module.scss';
@@ -14,16 +14,26 @@ const HeaderComponent = ({ onClickLogo, rightElements }: Props) => {
       <div className={styles.flexItem}>
         <Link
           href="/"
-          onClick={onClickLogo}
-          className={styles.box}
-          aria-label="홈으로 이동"
+          // v 13 사용시
+          // onClick={onClickLogo}
+          // className={styles.box}
+          // aria-label="홈으로 이동"
+          // 내부 링크 사용시
+          legacyBehavior
+          passHref
         >
-          <Image
+          {/* <Image
             src="/logo.png"
             width={20}
             height={20}
             alt="로고"
             style={{ objectFit: 'contain' }}
+          /> */}
+          <CustomAnchor
+            // v 12
+            onClick={onClickLogo}
+            className={styles.box}
+            aria-label="홈으로 이동"
           />
         </Link>
       </div>
@@ -33,3 +43,26 @@ const HeaderComponent = ({ onClickLogo, rightElements }: Props) => {
 };
 
 export default HeaderComponent;
+
+/**
+ * https://nextjs.org/docs/pages/api-reference/components/link#if-the-child-is-a-custom-component-that-wraps-an-a-tag
+ * https://nextjs.org/docs/pages/api-reference/components/link#if-the-child-is-a-functional-component
+ *  */
+// `onClick`, `href`, and `ref` need to be passed to the DOM element
+// for proper handling
+const CustomAnchor = React.forwardRef(function CustomAnchor(
+  props: ComponentPropsWithoutRef<'a'>,
+  ref: ForwardedRef<HTMLAnchorElement>
+) {
+  return (
+    <a {...props} ref={ref}>
+      <Image
+        src="/logo.png"
+        width={20}
+        height={20}
+        alt="로고"
+        style={{ objectFit: 'contain' }}
+      />
+    </a>
+  );
+});
