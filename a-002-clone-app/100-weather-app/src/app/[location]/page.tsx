9fd05a229a525612af206abf202490530bf9dad8
git-k <kgunu93@gmail.com>
2024-01-11 22:02:17 +0900

    feat: add error loading api

--- a-002-clone-app/100-weather-app/src/app/[location]/page.tsx
+++ a-002-clone-app/100-weather-app/src/app/[location]/page.tsx
@@ -6,8 +6,23 @@ type Props = {
   };
 };
 
-export default function Detail({ params }: Props) {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const getCurrentWeather = async (location: string): Promise<Response> => {
+  const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no&lang=ko`);
+  console.log(res);
+
+  if (!res.ok) {
+    throw new Error('날씨 정보를 가져올 수 없습니다.');
+  }
+
+  return res.json();
+};
+
+export default async function Detail({ params }: Props) {
   const name = params.location;
+  const res = await getCurrentWeather(name);
+  console.log(res);
 
   return (
     <>
