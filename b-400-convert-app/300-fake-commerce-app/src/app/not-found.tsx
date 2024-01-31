08ec163a29c2987f63658ad44d3e4726d712a4c7
git-k <kgunu93@gmail.com>
2024-01-31 21:44:48 +0900

    404 page

---
+++ b-400-convert-app/300-fake-commerce-app/src/app/not-found.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2 className="text-center">길을 잃었다~ 어딜 가야 할까~</h2>
+      <p>
+        <Link href="/">홈으로 이동</Link>
+      </p>
+    </div>
+  )
+}
