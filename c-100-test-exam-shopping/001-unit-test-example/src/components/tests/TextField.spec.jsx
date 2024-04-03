29d82c04004af3ba45beaccc8d5074a70a7a2dd0
git-k <kgunu93@gmail.com>
2024-04-03 21:27:53 +0900

    feat: add classname test

--- c-100-test-exam-shopping/001-unit-test-example/src/components/tests/TextField.spec.jsx
+++ c-100-test-exam-shopping/001-unit-test-example/src/components/tests/TextField.spec.jsx
@@ -4,6 +4,17 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+/**
+ * AAA 패턴
+ * Arrange - 테스트 환경 세팅
+ * Act - 테스트 동작 발생
+ * Assert - 올바른 동작 실행되었는지 검증
+ */
+
+it('className prop으로 설정한 css class가 적용된다.', async () => {
+  await render(<TextField className="my-class" />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  expect(textInput).toHaveClass('my-class');
 });
